eeaef36d4f8b694cbf0ed58221d0c0aa9e15e323
Omkar Patil <git21cs072-t@students.git.edu>
2024-03-18 22:51:04 +0530

    Some chaned

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -22,7 +22,7 @@ const [isMenuOpen, setIsMenuOpen] = useState(false);
             <Link to="/" className="flex items-center">
               {/* Add your logo here */}
               <img
-                src="/dist/assets/logo.png"
+                src="../../../Images/assets/logo.png"
                 className="hidden lg:block mr-3 h-16 hover:scale-110 duration-150 rounded-full"
                 alt="Logo"
               />
